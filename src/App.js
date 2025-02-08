c3d80a72d762ab1e7d2fe1528e06cbd3841464e4
daemon403 <nicholasmutsaka@gmail.com>
2025-02-08 04:42:54 +0000

    About section grids

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
+
 export default function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
 
   )
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name = 'about' className='h-screen bg-[#060312] text-white W-full flex flex-col justify-center items-center'>
+    <div name='about' className = 'w-full h-screen bg-blue-400 text-gray-400'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[90%] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p>
+            I am a passionate tech enthusiast with a deep curiosity 
+            for various aspects of technology. From data engineering 
+            and machine learning to backend development and cloud architecture, 
+            I enjoy exploring and building innovative solutions. Currently, I’m 
+            focused on developing the backend for my edtech startup using Node.js 
+            and JavaScript, while leveraging Google Cloud for database 
+            architecture. I thrive on solving complex problems, optimizing 
+            systems, and staying up-to-date with the latest advancements in tech.
+            </p>
+          </div>
+        
+        </div>
 
+      </div>
       
     </div>
   )
