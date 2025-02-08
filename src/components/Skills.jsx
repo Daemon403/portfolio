56ef15e6c8ac5dabfccefaf815d0deb27eca3574
daemon403 <nicholasmutsaka@gmail.com>
2025-02-08 05:05:23 +0000

    added skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className='' name='skills'>
+        <div className='w-full h-screen bg-[#060312] text-gray-400'>
+            <div className='flex flex-col justify-center items-center w-full h-full'>
+                <div className='max-w-[90%] w-full grid grid-cols-2 gap-8'>
+                    <div className='sm:text-right pb-8 pl-4'>
+                        <p className='text-4xl font-bold inline border-b-4 border-yellow-400'>
+                    Skills
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Skills
