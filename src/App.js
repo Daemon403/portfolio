eb30d8e9eab608cb66e12ae7d367cc6b73783880
daemon403 <nicholasmutsaka@gmail.com>
2025-02-11 23:45:07 +0000

    Added and imported Contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Home />
       <About />
       <Skills />
+      <Contact />
     </div>
 
   )
